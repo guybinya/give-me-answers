950339c8efe71a4d7e5549b8836674f39a54aa60
Guy Binya <guybinya@gmail.com>
2024-09-23 18:10:25 +0300

    Added pricing calcs

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,44 @@ import { zodResponseFormat } from 'openai/helpers/zod';
 import { z } from 'zod';
 import * as ChatAPI from 'openai/src/resources/chat/chat';
 
+type ChatModelPrices = {
+	inputPrice: number;
+	outputPrice: number;
+}
+
+// Updated on 2024-09-23
+const modelPrices: {[index: string | ChatAPI.ChatModel]: ChatModelPrices} = {
+	'gpt-4o': {
+		inputPrice: 0.000005,
+		outputPrice: 0.000015,
+	},
+	'gpt-4o-mini': {
+		inputPrice: 0.00000015,
+		outputPrice: 0.0000006,
+	},
+}
+
+const calculatePrice = (usage: OpenAI.Completions.CompletionUsage, model: string | ChatAPI.ChatModel) => {
+	const modelPrice = modelPrices[model];
+	if (!modelPrice) {
+		return null;
+	}
+	const pricing = {
+		prompt_tokens: usage.prompt_tokens,
+		prompt_price: usage.prompt_tokens * modelPrice.inputPrice,
+		completion_tokens: usage.completion_tokens,
+		completion_price: usage.completion_tokens * modelPrice.outputPrice,
+		total_tokens: usage.prompt_tokens + usage.completion_tokens,
+		total_price: usage.prompt_tokens * modelPrice.inputPrice + usage.completion_tokens * modelPrice.outputPrice
+	};
+	return pricing;
+};
+
 export const openAiRequestWithSchema = async <T>(
 	model: string | ChatAPI.ChatModel,
 	messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[],
-	schema: z.ZodType<T>
+	schema: z.ZodType<T>,
+	showPrices = false
 ): Promise<T | null> => {
 	const client = new OpenAI();
 
@@ -17,6 +51,9 @@ export const openAiRequestWithSchema = async <T>(
 			messages,
 			response_format: responseFormat
 		});
+		if (completion.usage && showPrices) {
+			console.log('Usage and Prices:', calculatePrice(completion.usage, model));
+		}
 		if (completion.choices[0].message.content === null) {
 			return null;
 		}
