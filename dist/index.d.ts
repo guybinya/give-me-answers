c82c72c961dc536363513f042fe0a35f398f1af0
Guy Binya <guybinya@gmail.com>
2024-09-23 18:10:41 +0300

    Added pricing calcs

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 import { z } from 'zod';
 import * as ChatAPI from 'openai/src/resources/chat/chat';
-export declare const openAiRequestWithSchema: <T>(model: string | ChatAPI.ChatModel, messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[], schema: z.ZodType<T>) => Promise<T | null>;
+export declare const openAiRequestWithSchema: <T>(model: string | ChatAPI.ChatModel, messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[], schema: z.ZodType<T>, showPrices?: boolean) => Promise<T | null>;
 export declare const giveMeAiMessages: ({ system, user }: {
     system: string;
     user: string;
--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.craftJsonSchemaPrompt = exports.giveMeAiMessages = exports.openAiRequestWithSchema = void 0;
 const openai_1 = require("openai");
 const zod_1 = require("openai/helpers/zod");
-const openAiRequestWithSchema = async (model, messages, schema) => {
+// Updated on 2024-09-23
+const modelPrices = {
+    'gpt-4o': {
+        inputPrice: 0.000005,
+        outputPrice: 0.000015,
+    },
+    'gpt-4o-mini': {
+        inputPrice: 0.00000015,
+        outputPrice: 0.0000006,
+    },
+};
+const calculatePrice = (usage, model) => {
+    const modelPrice = modelPrices[model];
+    if (!modelPrice) {
+        return null;
+    }
+    const pricing = {
+        prompt_tokens: usage.prompt_tokens,
+        prompt_price: usage.prompt_tokens * modelPrice.inputPrice,
+        completion_tokens: usage.completion_tokens,
+        completion_price: usage.completion_tokens * modelPrice.outputPrice,
+        total_tokens: usage.prompt_tokens + usage.completion_tokens,
+        total_price: usage.prompt_tokens * modelPrice.inputPrice + usage.completion_tokens * modelPrice.outputPrice
+    };
+    return pricing;
+};
+const openAiRequestWithSchema = async (model, messages, schema, showPrices = false) => {
     const client = new openai_1.default();
     const responseFormat = (0, zod_1.zodResponseFormat)(schema, 'schema');
     try {
@@ -12,6 +38,9 @@ const openAiRequestWithSchema = async (model, messages, schema) => {
             messages,
             response_format: responseFormat
         });
+        if (completion.usage && showPrices) {
+            console.log('Usage and Prices:', calculatePrice(completion.usage, model));
+        }
         if (completion.choices[0].message.content === null) {
             return null;
         }
